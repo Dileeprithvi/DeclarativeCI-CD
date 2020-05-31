8c53aaf49d51e5a76034b97a878a4c9542d239b9
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-05-31 17:31:56 +0530

    Added comments

--- delete_cont.sh
+++ delete_cont.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-sudo docker stop SpringbootApp || true	    
-sudo docker rmi -f $(docker images) || true  
+
+# Added || true - The true command's sole purpose is to return a successful exit status.
+
+# Stopping the Docker container SpringbootApp if it running
+sudo docker stop SpringbootApp || true	
+
+# Removing all the Docker images 
+sudo docker rmi -f $(docker images) || true 
+
+# Removing the Docker container SpringbootApp
 sudo docker rm -f SpringbootApp || true  
