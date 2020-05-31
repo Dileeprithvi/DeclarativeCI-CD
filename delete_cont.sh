8648536c35105e1f0c1ec3a6192d43fbb28fe566
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-05-31 16:58:27 +0530

    Update delete_cont.sh

--- delete_cont.sh
+++ delete_cont.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ "$(docker ps -q -f name=SpringbootApp)" ]; then
-        echo "Deleting the container"
-        docker rm -f SpringbootApp
-    else
-    echo " no container running"
-fi
+sudo docker stop SpringbootApp || true'	    
+sudo docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | egrep 'springboot|tomcat') || true'	    
+sudo docker rm -f SpringbootApp || true'  
