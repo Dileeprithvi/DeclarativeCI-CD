c476b0842f05609309eea4133c9b5daa711c5b2e
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-05-31 17:00:52 +0530

    Update delete_cont.sh

--- delete_cont.sh
+++ delete_cont.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker stop SpringbootApp || true'	    
-sudo docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | egrep 'springboot|tomcat') || true'	    
-sudo docker rm -f SpringbootApp || true'  
+sudo docker stop SpringbootApp || true	    
+sudo docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | egrep 'springboot|tomcat') || true    
+sudo docker rm -f SpringbootApp || true  
