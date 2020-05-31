743fa662fcbd98ef8b63f2e140c094fa048b4c8c
Dileep Prithvi <34166409+Dileeprithvi@users.noreply.github.com>
2020-05-31 12:08:55 +0530

    Delete delete_cont.sh

--- #!/bin/delete_cont.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$(docker ps -q -f name=SpringbootApp)" ]; then
-        echo "Deleting the container"
-        docker rm -f SpringbootApp
-    else
-    echo " no container running"
-fi
